49576e8de63529dcb3c6f649c14e594a3ea0e8b7
Sparsh Jain <sparshjain325@gmail.com>
2025-01-21 07:16:37 +0530

    jsx and babel

--- something.js
+++ something.js
@@ -1,7 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-let hTag = React.createElement("h1", {
-    id: "somecss"
-}, "CHALO BHAI");
+
+
+
+let jsxHeading = <h1 className="myClass">Hello I am a jsx tag!</h1>
+console.log('jsxHeading: ', jsxHeading);
+// jsxHeading -> react Element using babel(transpiling) -> js(ES6) -> browser 
+// attributes will be in a camel case in jsx like className
+
+// let hTag = React.createElement("h1", {
+//     id: "somecss"
+// }, "CHALO BHAI");
 let reactDom = ReactDOM.createRoot(document.getElementById("root"));
-reactDom.render(hTag)
+reactDom.render(jsxHeading)
