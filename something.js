bfd7349bd9629d0a93873a4b53f4cc6cd66c56d7
Sparsh Jain <sparshjain325@gmail.com>
2025-01-23 23:06:31 +0530

    Fast Food App

--- something.js
+++ something.js
@@ -14,23 +14,103 @@ import ReactDOM from "react-dom/client";
 //     id: "somecss"
 // }, "CHALO BHAI");
 
-let ComponentOne = () => {
-    return <h1> This is First Component made using {} and return</h1>
-};
+// let ComponentOne = () => {
+//     return <h1> This is First Component made using {} and return</h1>
+// };
 
-let ComponentThree = () => <h1>This is the third component made without anything</h1>
+// let ComponentThree = () => <h1>This is the third component made without anything</h1>
 
-let ComponentTwo = () => (
-    <div id="container">
-        <ComponentOne />
-        <h1>This is the second component made using just ()</h1>
-        <ComponentThree />
-    </div>
-);
+// const someInteger = 100;
+
+// let ComponentTwo = () => (
+//     <div id="container">
+//         {someInteger} 
+//         <ComponentOne />
+//         <h1>This is the second component made using just ()</h1>
+//         <ComponentThree />
+//     </div>
+// );
+
+let RestaurantCard = () => {
+    return (
+        <div className="restaurantCard">
+            RestaurantCard
+        </div>
+    )
+}
+
+let Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <img width="100" height="100" src="https://img.icons8.com/clouds/100/street-food.png" alt="street-food"/>
+            </div>
+            <div className="navItems">
+                <ul className="itemList">
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        About Us
+                    </li>
+                    <li>
+                        Cart
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+let Body = () => {
+    return (
+        <div className="body">
+            <div className="searchBar">
+                <span>Search</span>
+                <img src="https://img.icons8.com/?size=100&id=132&format=png&color=000000" className="searchIcon" />
+            </div>
+            <div className="restaurants">
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+let Footer = () => {
+    return (
+        <div className="footer">
+            Footer
+        </div>
+    )
+}
+
+let App = () => {
+    return (
+        <div id="app">
+            {/* Header
+            Body
+            Footer */}
+
+            <Header />
+            <Body />
+            <Footer />
+        </div>
+    )
+}
 
 // Component Two is an example of Compoenent Composition 
 
 let reactDom = ReactDOM.createRoot(document.getElementById("root"));
 // reactDom.render(jsxHeading)
 
-reactDom.render(<ComponentTwo />);
+reactDom.render(<App />);
